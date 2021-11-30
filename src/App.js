117a4f2a110af20cdeb3fe8f2b42e452e2105943
thiagobrsouza <tbs.souza@outlook.com>
2021-11-30 16:07:20 -0300

    Finalizando navbar e footer

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home'
 import NewProject from './components/pages/NewProject';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Sobre</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </div>
+      <Navbar />
       <Switch>
         <Container customClass="min-height">
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
           </Route>
           <Route exact path="/company">
             <Company />
@@ -30,7 +31,7 @@ function App() {
           </Route>
         </Container>
       </Switch>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }

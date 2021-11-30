2464155799100ec373eb785499ecb09e795afd12
thiagobrsouza <tbs.souza@outlook.com>
2021-11-30 17:22:48 -0300

    Finalizando layout do formulario de novo projeto

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,19 +1,22 @@
-export default function ProjectForm() {
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+import styles from './ProjectForm.module.css'
+
+export default function ProjectForm({btnText}) {
   return (
-    <form>
+    <form className={styles.form}>
       <div>
-        <input type="text" placeholder="Insira o nome do projeto"/>
+        <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do projeto"/>
       </div>
       <div>
-        <input type="number" placeholder="Insira o orçamento total"/>
+      <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento do Projeto"/>
       </div>
       <div>
-        <select name="category_id">
-          <option disabled>Selecione a categoria</option>
-        </select>
+        <Select name="category_id" text="Selecione a categoria"/>
       </div>
       <div>
-        <input type="submit" value="Criar projeto"/>
+        <SubmitButton text={btnText}/>
       </div>
     </form>
   )

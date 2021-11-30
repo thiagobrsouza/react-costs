65243f572c257c22d47572fe6d2044f344cd9ed6
thiagobrsouza <tbs.souza@outlook.com>
2021-11-30 17:41:37 -0300

    Criando fake api para trazer categorias no select

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -3,7 +3,26 @@ import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
+import {useEffect, useState} from 'react'
+
 export default function ProjectForm({btnText}) {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <div>
@@ -13,7 +32,7 @@ export default function ProjectForm({btnText}) {
       <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento do Projeto"/>
       </div>
       <div>
-        <Select name="category_id" text="Selecione a categoria"/>
+        <Select name="category_id" text="Selecione a categoria" options={categories}/>
       </div>
       <div>
         <SubmitButton text={btnText}/>

9a19fdd16fe2a59e0884b06789aa89b4c1a99cc5
thiagobrsouza <tbs.souza@outlook.com>
2021-11-30 15:36:20 -0300

    Adicionando as rotas e seus arquivos de rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Container from './components/layout/Container';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Sobre</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </div>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
